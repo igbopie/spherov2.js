1aa725f1dbeea02bb78d5aed03bbb72ae0c4268c
Craig Walls <craig@habuma.com>
2018-08-11 15:30:02 -0600

    Change while loop to for loop in ensemble sample

---
+++ lib/examples/ensemble.d.ts
@@ -0,0 +1,2 @@
+import { SpheroMini } from '../toys/sphero-mini';
+export declare const ensemble: (minis: SpheroMini[]) => Promise<void>;
--- src/examples/ensemble.ts
+++ src/examples/ensemble.ts
@@ -1,40 +1,34 @@
 import { SpheroMini } from '../toys/sphero-mini';
 import { wait } from '../utils';
-import { findSpheroMini } from './lib/scanner';
 import { findToys } from './lib/scanner';
 
-import { Core } from '../../toys/core';
-import { Peripheral } from 'noble';
-import * as noble from 'noble';
-
-import { IToyAdvertisement } from '../../toys/types';
+import { Core } from '../toys/core';
+// import * as noble from 'noble';
 
 const WAIT_TIME: number = 100;
 
 export const ensemble = async (minis: SpheroMini[]) => {
-  var i=0;
   // Cycle 10 times. On each cycle set each bot to white, green, then red.
-  while (i < 10) {
+  for (let i = 0; i < 10; i++) {
     // set each mini's color to white
-    for (let mini of minis) {
+    for (const mini of minis) {
       await mini.setMainLedColor(0xFF, 0xFF, 0xFF);
       await wait(WAIT_TIME);
     }
     // set each mini's color to green
-    for (let mini of minis) {
+    for (const mini of minis) {
       await mini.setMainLedColor(0, 0xFF, 0);
       await wait(WAIT_TIME);
     }
     // set each mini's color to red
-    for (let mini of minis) {
+    for (const mini of minis) {
       await mini.setMainLedColor(0xFF, 0, 0);
       await wait(WAIT_TIME);
     }
-    i++;
   }
 
   // Put each mini to sleep
-  for (let mini of minis) {
+  for (const mini of minis) {
     await mini.sleep();
   }
   process.exit();
@@ -46,17 +40,17 @@ const main = async () => {
 
   // sort the minis by their bluetooth advertisment local name (e.g, "SM-????")
   bots.sort((a, b): number => {
-    if (a.peripheral.advertisement.localName < b.peripheral.advertisement.localName)
+    if (a.peripheral.advertisement.localName < b.peripheral.advertisement.localName) {
       return -1;
-    if (a.peripheral.advertisement.localName > b.peripheral.advertisement.localName)
+    } else if (a.peripheral.advertisement.localName > b.peripheral.advertisement.localName) {
       return 1;
+    }
     return 0;
   });
 
   // start each bot, cast it to a SpheroMini object, then add to an array
-  let minis = [];
-  for(let bot of bots) {
-    console.log("Starting: " + bot.peripheral.advertisement.localName);
+  const minis = [];
+  for (const bot of bots) {
     // cast bot to a Core object
     const toy: Core = new SpheroMini.advertisement.class(bot.peripheral);
     await toy.start();
@@ -69,6 +63,5 @@ const main = async () => {
   // run the ensemble action on the array of minis
   ensemble(minis);
 };
-
 
 main();
