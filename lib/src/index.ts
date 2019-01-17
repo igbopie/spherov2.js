82ed0a3fbe54fdf70a04a55fa37f7bf05bb227dc
Ignacio Bona <igbopie@gmail.com>
2019-01-17 07:54:19 -0800

    Fixing npm package

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,9 +1,22 @@
-import { SpheroMini } from './toys/sphero-mini';
-import { LightningMcQueen } from './toys/lightning-mcqueen';
-import { BB9E } from './toys/bb9e';
+export { RollableToy } from './toys/rollable-toy';
+export { BB9E } from './toys/bb9e';
+export { LightningMcQueen } from './toys/lightning-mcqueen';
+export { R2D2 } from './toys/r2d2';
+export { R2Q5 } from './toys/r2q5';
+export { SpheroMini } from './toys/sphero-mini';
+export { Core } from './toys/core';
+export { Event } from './toys/core';
+export { Stance } from './toys/types';
+export { ICommandWithRaw } from './commands/types';
 
-export {
-  SpheroMini,
-  LightningMcQueen,
-  BB9E,
-};
+import * as scanner from './scanner';
+import * as utils from './utils';
+
+/**
+ * Use Scanner to find toys
+ */
+export const Scanner = scanner;
+/**
+ * Utility functions
+ */
+export const Utils = utils;
