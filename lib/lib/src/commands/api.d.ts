e39142fdbb2a06338ac6b2b3307bbee8c7dc725a
Ignacio Bona <igbopie@gmail.com>
2019-01-17 07:08:48 -0800

    Added prepublish step, renamed lib to dist, reorg docs

--- lib/lib/src/commands/api.d.ts
+++
@@ -1,5 +0,0 @@
-import { CommandGenerator, ICommandWithRaw } from './types';
-declare const _default: (generator: CommandGenerator) => {
-    echo: () => ICommandWithRaw;
-};
-export default _default;
--- lib/lib/src/toys/bb9e.d.ts
+++
@@ -1,5 +0,0 @@
-import { IToyAdvertisement } from './types';
-import { RollableToy } from './rollable-toy';
-export declare class BB9E extends RollableToy {
-    static advertisement: IToyAdvertisement;
-}
