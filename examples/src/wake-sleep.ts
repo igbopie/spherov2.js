4ea004a73887e76a11bf83a1b897f02bd9854d8a
Ignacio Bona <igbopie@gmail.com>
2019-03-16 00:07:38 -0700

    empty

--- examples/src/wake-sleep.ts
+++ examples/src/wake-sleep.ts
@@ -1,7 +1,7 @@
 import { starter } from './utils/starter';
 import { wait } from 'spherov2.js';
-import { RollableToy } from 'spherov2.js';
 import { exit } from 'shelljs';
+import { RollableToy } from 'spherov2.js';
 
 export const wakeSleep = async (toy: RollableToy) => {
   await toy.setMainLedColor(255, 0, 0);
